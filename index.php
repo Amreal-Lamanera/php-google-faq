175175b0ba75824389f0fa092f4b079486310c3d
Francesco Pieraccini <pieraccini.francesco@gmail.com>
2022-09-22 16:13:55 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google faq</title>
+    <link rel="stylesheet" href="css/app.css">
+</head>
+
+<body>
+
+    <header>
+        <div class="logo">
+            <img src="./img/google-logo.png" alt="">
+            <h1>Privacy e termini</h1>
+        </div>
+
+        <nav>
+            <ul class="list-wrapper">
+                <a href="#nogo">
+                    <li class="list-item">
+                        Introduzione
+                    </li>
+                </a>
+
+                <a href="#nogo">
+                    <li class="list-item">
+                        Norme sulla privacy
+                    </li>
+                </a>
+
+                <a href="#nogo">
+                    <li class="list-item">
+                        Termini di servizio
+                    </li>
+                </a>
+
+                <a href="#nogo">
+                    <li class="list-item">
+                        Tecnologie
+                    </li>
+                </a>
+
+                <a href="#nogo">
+                    <li class="list-item active">
+                        Domande frequenti
+                    </li>
+                </a>
+
+            </ul>
+        </nav>
+    </header>
+
+</body>
+
+</html>
